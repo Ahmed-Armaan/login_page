22e32d2733d7dbd1139d7cd6d8602f763b88f09f
Ahmed-Armaan <ahmedarmaan2022@gmail.com>
2023-09-16 19:56:44 +0530

    basic working added

---
+++ dist/public/script.js
@@ -0,0 +1,16 @@
+// // script.js
+// document.getElementById("login-form").addEventListener("submit", function(event) {
+//     event.preventDefault();
+//     const username = document.getElementById("login-username").value;
+//     const password = document.getElementById("login-password").value;
+//     // Perform login validation here (e.g., check credentials with a server).
+//     console.log("Login submitted with username:", username, "and password:", password);
+// });
+
+// document.getElementById("signup-form").addEventListener("submit", function(event) {
+//     event.preventDefault();
+//     const username = document.getElementById("susername").value;
+//     const password = document.getElementById("signup-password").value;
+//     // Perform signup validation here (e.g., create a new user on the server).
+//     console.log("Signup submitted with username:", username, "and password:", password);
+// });
